b5f7a7260ef66d985c5f502deb6b385dbba140bc
vaas2k <sk315425@gmail.com>
2024-05-01 11:13:06 +0500

    --

--- app/api/protectedRoute/route.js
+++ app/api/protectedRoute/route.js
@@ -10,9 +10,4 @@ async function handler(req, res) {
     }
   });
 }
-
-<<<<<<< HEAD
 export { handler as GET, handler as POST}
-=======
-export { handler as GET, handler as POST}
->>>>>>> 6eb6e28a4e93876b6ac1cd70510bde0114617d43
